11756e0884920efe41d36aaeda003f70cfdc2ce0
Lynn <landiao_lr@qq.com>
2017-04-23 17:19:45 +0800

    add delete this

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div id="app">
     <h1>{{title}}</h1>
     <input type="text" id="write" v-model="newItem" @keyup.enter="addNew" >
+    <button @click="addNew">{{submit}}</button>
     <form>
-      <div v-for="item in items">
+      <div v-for="(item, index) in items">
         <label :class="{finished: item.isFinished}">
           <input type="checkbox" :checked="item.isFinished" @click="toggleFinish(item)"/>
           {{item.label}}
         </label>
+        <span @click="delThis(index)">x</span>
       </div>
     </form>
     <div id="clc">
@@ -23,6 +25,7 @@ export default {
   data () {
     return {
       title: 'Todo List',
+      submit: '提交',
       clc: 'Clear All',
       clcF: 'Clear Finished',
       items: Store.fetch(),
@@ -42,9 +45,11 @@ export default {
       item.isFinished = !item.isFinished;
     },
     addNew () {
-      this.items.unshift({label: this.newItem, isFinished: false});
-      //console.log(this.newItem);
-      this.newItem = '';
+      if (this.newItem) {
+        this.items.unshift({label: this.newItem, isFinished: false});
+        //console.log(this.newItem);
+        this.newItem = '';
+      }
     },
     reset () {
       this.items = [];
@@ -56,6 +61,9 @@ export default {
           this.items.splice(i, 1);
         }
       }
+    },
+    delThis (index) {
+      this.items.splice(index, 1);
     }
   }
 }
@@ -78,7 +86,7 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   /*color:  #2E8B57; */ /*#2c3e50;*/
-  margin-top: 80px;
+  margin-top: 10%;
   width: 80%;
 }
 h1 {
@@ -86,25 +94,36 @@ h1 {
   color:  #2E8B57;
 }
 #write {
-  width: 100%;
-  height: 1.5rem;
+  width: 72%;
+  height: 2rem;
   margin: 20px 0;
   border: 1px solid #ccc;
-  /*border-radius: 2px;*/
+  border-radius: 0.2rem;
 }
 #write:focus {
   outline: 0;
-  box-shadow: 0 0 3px #39B662;
+  border: 1px solid #39B662;
+  box-shadow: 0 0 4px #39B662;
+}
+button {
+  margin-left: 2%;
+  vertical-align: middle;
+  width: 23%;
+  height: 2rem;
+  background-color: #3CB371;
+  border: 0;
+  color: #FFF;
+  border-radius: 0.3rem;
 }
 form div {
   color:  #000;
   text-align: left;
-  font-size: 1.4rem;
+  font-size: 1.1rem;
   margin: 0.6rem 0;
 }
 label {
   display: inline-block;
-  width: 100%;
+  width: 93%;
 }
 label input {
   width: 1.2rem;
@@ -125,11 +144,11 @@ label input {
   color: #FFF;
   height: 2rem;
 }
-span:first-child {
+#clc span:first-child {
   border-right: 1px solid #FFF;
   padding-right: 3%;
 }
-span {
+#clc span {
   display: inline-block;
   width: 40%;
   height: 2rem;
